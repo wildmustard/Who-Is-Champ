7aee3e810303f5e8a03780b7ba916fc7904b7976
Alex Clark <aclark2089@gmail.com>
2015-10-24 12:26:30 -0400

    Initial Repo Push

---
+++ client/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('whoIsChamp', [
+  'ngRoute',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngAnimate'
+])
+  .config(function ($routeProvider, $locationProvider) {
+
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+
+  });
