bfac9dc1e78d7be34a8c8d0b17eaaf5e2366c43e
Alex Clark <aclark2089@gmail.com>
2015-10-24 17:52:44 -0400

    Added video player

--- client/directives/header/header.controller.js
+++ client/directives/header/header.controller.js
@@ -1,3 +1,25 @@
-/**
- * Created by Alex on 10/24/15.
- */
+'use strict';
+
+angular.module('whoIsChamp')
+    .controller('HeaderCtrl', ["$sce", function ($sce) {
+        this.config = {
+            sources: [
+                {src: $sce.trustAsResourceUrl("assets/videos/JOHNCENA.mp4"), type: "video/mp4"},
+                {src: $sce.trustAsResourceUrl("assets/videos/TheChamp.mp4"), type: "video/mp4"},
+                {src: $sce.trustAsResourceUrl("assets/videos/YouCantSee.mp4"), type: "video/mp4"}
+            ],
+            tracks: [
+                {
+                    src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
+                    kind: "subtitles",
+                    srclang: "en",
+                    label: "English",
+                    default: ""
+                }
+            ],
+            theme: "bower_components/videogular-themes-default/videogular.css",
+            plugins: {
+                poster: ""
+            }
+        };
+    }]);
