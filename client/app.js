0217cdd908dff8cd950d47dac8967a2ce7285a18
Alex Clark <aclark2089@gmail.com>
2015-10-24 12:49:35 -0400

    Updated Dependencies

--- client/app.js
+++ client/app.js
@@ -1,19 +1,25 @@
 'use strict';
 
 angular.module('whoIsChamp', [
-  'ngRoute',
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngAnimate'
+    'ngRoute',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngAnimate',
+    'ui.router',
+    'restangular',
+    'com.2fdevs.videogular',
+    'com.2fdevs.videogular.plugins.controls',
+    'com.2fdevs.videogular.plugins.overlayplay',
+    'com.2fdevs.videogular.plugins.poster'
 ])
-  .config(function ($routeProvider, $locationProvider) {
+    .config(function ($routeProvider, $locationProvider) {
 
-    $routeProvider
-      .otherwise({
-        redirectTo: '/'
-      });
+        $routeProvider
+            .otherwise({
+                redirectTo: '/'
+            });
 
-    $locationProvider.html5Mode(true);
+        $locationProvider.html5Mode(true);
 
-  });
+    });
